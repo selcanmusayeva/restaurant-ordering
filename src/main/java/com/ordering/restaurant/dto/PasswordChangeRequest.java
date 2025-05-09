fb921da022d025e725e5962bb88c359785bb93b9
Melek Hajili <95547000+melekhajili@users.noreply.github.com>
2025-05-09 09:07:46 +0400

    Create authentication DTOs

---
+++ src/main/java/com/ordering/restaurant/dto/PasswordChangeRequest.java
@@ -0,0 +1,24 @@
+package com.ordering.restaurant.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordChangeRequest {
+    
+    @NotBlank(message = "Current password is required")
+    private String currentPassword;
+    
+    @NotBlank(message = "New password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String newPassword;
+    
+    @NotBlank(message = "Password confirmation is required")
+    private String confirmPassword;
+} 
