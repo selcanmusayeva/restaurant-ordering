2b4f5877d46889f05a6ff75993bc65d66786f73a
Ismayil_Ismayilov_18008 <iismayilov180008@ada.edu.az>
2025-05-11 09:06:34 +0400

    Create custom user details service

---
+++ src/main/java/com/ordering/restaurant/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.ordering.restaurant.security;
+
+import com.ordering.restaurant.model.User;
+import com.ordering.restaurant.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return user;
+    }
+}
